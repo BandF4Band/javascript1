cf47c833c0c7f10ac2b4c855d019c4490c2181dd
BandF4Band <149424152+BandF4Band@users.noreply.github.com>
2023-11-09 15:25:44 -0500

    Create unit1.js

---
+++ unit1.js
@@ -0,0 +1,41 @@
+let colorlist = ['red', 'blue', 'orange', 'yellow']
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255, 255, 255);
+}
+
+function draw () {
+  ellipse(mouseX, mouseY, 25, 25);
+  
+//quad(30,45,20,79,120,40,255,115);
+
+//triangle(20,55,140,240,90,68);
+
+
+  
+  //strokeWeight(12);
+  //stroke(120,40,58);
+  //line(30,0,width,height);
+
+ // strokeWeight(8);
+  //stroke(0,0,232);
+  //point(130,221);
+  //point(155,249);
+
+
+
+
+  
+  //angleMode(DEGREES); 
+//  strokeWeight(1);
+ // fill(200,15,0)
+  //arc(120, 50, 100, 100, 0, 240, CHORD); 
+ // strokeWeight(15);
+  //fill(290,7,0)
+  //arc(250, 50, 100, 100, 0, 240, PIE); 
+  //strokeWeight(5);
+  //fill(100,5,0)
+  //arc(350, 310, 200, 200, 0, 240, OPEN);  
+}
+
